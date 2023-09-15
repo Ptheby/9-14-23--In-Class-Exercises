b9d51ab855c390c407ab0c07c90e709387ed39c5
Paul Theby <paultheby@gmail.com>
2023-09-15 12:42:09 -0500

    Exercise 1 From in Class 9/14/23
    Called office hours and Mike helped me break down the whole process from scratch . Thank you

---
+++ InClassObserverExample/main.js
@@ -0,0 +1,56 @@
+class Subject {
+	constructor() {
+		this.observers = []; //this is state as an array
+	}
+	// Add methods here ///methods are the behavior the classes act upon
+  addObserver(observer){
+      this.observers.push(observer);
+     
+  }
+  removeObserver(observer){
+    this.observers = this.observers.filter((obs)=> obs !== observer)
+
+  }
+  notifyObservers() {
+    this.observers.forEach((observer)=> observer.update());
+    console.log("This is our array:",this.observers)
+    //forEach built in 
+  }
+}
+
+class Observer {
+	update() {
+		console.log('Observer updated!');
+	}
+}
+const subject = new Subject(); 
+ //instantiate our object---created an object
+//create new object based on Subject class
+const observer1= new Observer()
+const observer2= new Observer()
+const observer3= new Observer()
+const observer4= new Observer()
+const observer5= new Observer()
+const observer6= new Observer()
+const observer7= new Observer() ///created 7 observers baesd on Observer class
+
+subject.addObserver(observer1);
+subject.addObserver(observer2);
+subject.addObserver(observer3);
+subject.addObserver(observer4);
+subject.addObserver(observer5);
+subject.addObserver(observer6);
+subject.addObserver(observer7);
+
+subject.notifyObservers();
+
+subject.removeObserver(observer1);
+subject.removeObserver(observer2);
+subject.removeObserver(observer3);
+
+subject.notifyObservers();
+
+
+
+
+
