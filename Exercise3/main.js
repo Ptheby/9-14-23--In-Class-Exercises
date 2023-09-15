e90aaa5ead531a0f33c5bff062990ff635aa0fd0
Paul Theby <paultheby@gmail.com>
2023-09-14 20:25:48 -0500

    update exercise 3

--- Exercise3/main.js
+++ Exercise3/main.js
@@ -1,7 +1,12 @@
 async function fetchPosts() {
 	const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
 
-	// Your async/await code here
+    try {
+        const response = await fetch(url);
+        
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
 }
 
 // Call the function to fetch posts
