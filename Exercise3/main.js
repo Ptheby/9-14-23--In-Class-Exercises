2370433ce01755c1fe92011400d2e1bf10e4ce08
Paul Theby <paultheby@gmail.com>
2023-09-14 20:21:03 -0500

    Exercise 3 starting

---
+++ Exercise3/main.js
@@ -0,0 +1,8 @@
+async function fetchPosts() {
+	const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
+
+	// Your async/await code here
+}
+
+// Call the function to fetch posts
+fetchPosts();
